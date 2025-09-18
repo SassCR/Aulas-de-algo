3df8f2717caa0285c2ea30e4da19deeb50f7824b
SassCR <renefodao123@gmail.com>
2025-09-17 20:53:42 -0400

    Add files via upload

---
+++ atividade8.js
@@ -0,0 +1,11 @@
+let promtpt = require=("prompt-sync")()
+let total,
+//Exercício 8: Dias para Idade
+//(Complexidade: Alta) - Faça um algoritmo que leia a idade de uma pessoa expressa em dias e mostre-a expressa em anos, meses e dias. (Use as mesmas considerações: ano com 365 dias, mês com 30 dias). Este exercício exige o uso do operador de módulo (%) para encontrar os restos.
+
+//Exemplo de Interação:
+//
+//Digite a idade total em dias: 9230
+//---
+//Saída no Console:
+//Idade: 25 anos, 3 meses e 5 dias.
